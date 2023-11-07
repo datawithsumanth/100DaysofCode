27933e5cb2c1731dab06baddca833a6511a71cc5
Sumanth S Kumar <sumanthsudarshankumar@gmail.com>
2023-11-06 19:17:27 -0600

    added invalid input condition to RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -28,21 +28,27 @@ scissors = ("""
 
 rps_list = [rock, paper, scissors]
 
-player_1_selection = int(input("Select 0 for Rock, 1 for Paper and 2 for Scissors\n"))
-print("You Chose")
-print(rps_list[player_1_selection])
+player_1_selection = (input("Select 0 for Rock, 1 for Paper and 2 for Scissors\n"))
 
-player_2_selection = random.randint(0, 2)
-print("Computer Chose")
-print(rps_list[player_2_selection])
 
-if player_1_selection == 0 and player_2_selection == 2:
-    print("You Win!")
-elif player_1_selection == 2 and player_2_selection == 1:
-    print("You Win!")
-elif player_1_selection == 1 and player_2_selection == 0:
-    print("You Win!")
-elif player_1_selection == player_2_selection:
-    print("It's a tie!")
+if player_1_selection == '0' or player_1_selection == '1' or player_1_selection == '2':
+    player_1_selection = int(player_1_selection)
+    print("You Chose")
+    print(rps_list[player_1_selection])
+
+    player_2_selection = random.randint(0, 2)
+    print("Computer Chose")
+    print(rps_list[player_2_selection])
+
+    if player_1_selection == 0 and player_2_selection == 2:
+        print("You Win!")
+    elif player_1_selection == 2 and player_2_selection == 1:
+        print("You Win!")
+    elif player_1_selection == 1 and player_2_selection == 0:
+        print("You Win!")
+    elif player_1_selection == player_2_selection:
+        print("It's a tie!")
+    else:
+        print("You Lose")
 else:
-    print("You Lose")
+    print("Invalid Input\nPlease choose 0, 1 or 2")
