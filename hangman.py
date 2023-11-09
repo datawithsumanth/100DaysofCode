7a12c47dab49234aad28f11c5e6417b9345c3618
Sumanth S Kumar <sumanthsudarshankumar@gmail.com>
2023-11-08 19:18:54 -0600

    Added simple hangman program

---
+++ hangman.py
@@ -0,0 +1,27 @@
+import random
+
+word_list = ["apple", "banana", "cherry", "date", "grape"]
+answer = list(random.choice(word_list))
+letters_guessed = ['_' for i in range(0, len(answer))]
+guesses_left = 6
+
+while answer != letters_guessed and guesses_left != 0:
+    guess = input("Please guess an alphabet\n")
+    print(" ".join(letters_guessed))
+    if guess in answer:
+        print(f"{guess} was present in the word")
+        for j in range(0, len(answer)):
+            if answer[j] == guess:
+                letters_guessed[j] = guess
+        print(" ".join(letters_guessed))
+    else:
+        guesses_left -= 1
+        print(f"Sorry, {guess} was not present in the word")
+        print(" ".join(letters_guessed))
+        print(f"You have {guesses_left} guesses left")
+
+
+if answer == letters_guessed:
+    print("You won!")
+else:
+    print("You lost!")
