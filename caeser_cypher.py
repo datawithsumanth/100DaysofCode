07a0690506a19728dbb349cd56884e657279d3fd
Sumanth S Kumar <sumanthsudarshankumar@gmail.com>
2023-11-09 07:49:41 -0600

    caeser_cypher.py
    - user interface made better
    - Combined encrypt and decrypt functions

--- caeser_cypher.py
+++ caeser_cypher.py
@@ -1,22 +1,27 @@
 import string
+from art import logo
 
 repeat = 'yes'
 alphabet_list = list(string.ascii_lowercase)
-encrypt = ''
-decrypt = ''
+print(logo)
+def caeser_cypher( msg, shift_no, selection):
+    cypher_text=''
+    if selection == 'decode':
+        shift_no *= -1 * shift_no
+
+    for i in msg:
+        if i in alphabet_list:
+            encrypt_no = (alphabet_list.index(i) + shift_no) % 26
+            cypher_text += (alphabet_list[encrypt_no])
+        else:
+            cypher_text += i
+    return cypher_text
+
 while repeat == 'yes':
     selection = input("type encode to encrypt and decode to decrypt\n").lower()
     msg = input("Type your message:\n").lower()
     shift_no = int(input("Type the shift number\n"))
-    if selection == 'encode':
-        for i in msg:
-            encrypt_no = (alphabet_list.index(i) + shift_no) % 26
-            encrypt += (alphabet_list[encrypt_no])
-        print(encrypt)
-    else:
-        for i in encrypt:
-            decrypt_no = (alphabet_list.index(i) - shift_no) % 26
-            decrypt += (alphabet_list[decrypt_no])
-        print(decrypt)
-
+    print(caeser_cypher(msg, shift_no, selection))
     repeat = input("Would you like to go again?\n")
+else:
+    print("Goodbye!")
