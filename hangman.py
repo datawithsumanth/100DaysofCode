0a399cf1b1cedc7a68d95d5d34b962eed7133081
Sumanth S Kumar <sumanthsudarshankumar@gmail.com>
2023-11-08 21:36:13 -0600

    Added hangman art

--- hangman.py
+++ hangman.py
@@ -1,11 +1,69 @@
 import random
+from replit import clear
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["apple", "banana", "cherry", "date", "grape"]
 answer = list(random.choice(word_list))
 letters_guessed = ['_' for i in range(0, len(answer))]
-guesses_left = 6
+guesses_left = 7
 
 while answer != letters_guessed and guesses_left != 0:
+    clear()
     guess = input("Please guess an alphabet\n")
     print(" ".join(letters_guessed))
     if guess in answer:
@@ -19,6 +77,7 @@ while answer != letters_guessed and guesses_left != 0:
         print(f"Sorry, {guess} was not present in the word")
         print(" ".join(letters_guessed))
         print(f"You have {guesses_left} guesses left")
+        print(stages[guesses_left])
 
 
 if answer == letters_guessed:
